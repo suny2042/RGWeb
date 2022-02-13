154f678c33553e3d765faf119144c0b0ebc103f3
suny2042 <suny2042@naver.com>
2022-02-13 14:10:27 +0900

    멀티 완성

--- RGWeb/ViewModels/ContentViewModel.cs
+++ RGWeb/ViewModels/ContentViewModel.cs
@@ -5,8 +5,8 @@ namespace RGWeb.ViewModels
     public class ContentViewModel : BaseViewModel, IContentViewModel
     {
         // 이곳을 static으로 선언시 모든 유저와 공유됨
-        private static ModelDataSet<Content> _oContentList = new ModelDataSet<Content>();
-        public ModelDataSet<Content> oContentList
+        private static List<ModelDataSet<Content>> _oContentList = new List<ModelDataSet<Content>>();
+        public List<ModelDataSet<Content>> oContentList
         {
             get => _oContentList;
             set => SetValue(ref _oContentList, value);
