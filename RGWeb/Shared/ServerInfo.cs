df4e095532701f462986711c526e9626fe14241a
suny2042 <suny2042@naver.com>
2022-02-20 15:23:30 +0900

    스케줄러 작성중

--- RGWeb/Shared/ServerInfo.cs
+++ RGWeb/Shared/ServerInfo.cs
@@ -13,14 +13,21 @@
         {
             public int Value { get; set; }
             public string Text { get; set; }
-            public int SiteType { get; set; } // 1 : 디시인사이드 2: 아카라이브
+            /// <summary>
+            /// 1: 디시인사이드 / 2: 아카라이브
+            /// </summary>
+            public int SiteType { get; set; }
+            /// <summary>
+            /// 0: 자동 / -1: 순차 / 1: 최우선 / 2: 2번중한번 / 3: 3번중한번 / ~
+            /// </summary>
+            public int RefreshLevel { get; set; }
             public string Url { get; set; }
         }
         public static List<PageType> PageTypeList = new List<PageType>
         {
-            new PageType() { Value = 0, SiteType = 1, Text = "국내야구", Url="https://m.dcinside.com/board/baseball_new10" },
-            new PageType() { Value = 1, SiteType = 1, Text = "만화", Url="https://m.dcinside.com/board/comic_new3" },
-            new PageType() { Value = 2, SiteType = 1, Text = "스트리머", Url="https://m.dcinside.com/board/stream_new1" },
+            new PageType() { Value = 0, SiteType = 1, RefreshLevel = 3, Text = "국내야구", Url="https://m.dcinside.com/board/baseball_new10" },
+            new PageType() { Value = 1, SiteType = 1, RefreshLevel = 3, Text = "만화", Url="https://m.dcinside.com/board/comic_new3" },
+            new PageType() { Value = 2, SiteType = 1, RefreshLevel = 3, Text = "스트리머", Url="https://m.dcinside.com/board/stream_new1" },
         };
     }
 }
